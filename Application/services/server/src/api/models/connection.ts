324ccdcffe23a146d53f64d3314a2e5163378874
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2020-04-14 10:12:36 +0200

    Added planning routes + database seeder

--- Application/services/server/src/api/models/connection.ts
+++ Application/services/server/src/api/models/connection.ts
@@ -1,8 +1,9 @@
 import {Sequelize} from 'sequelize-typescript';
+import seedDatabase from './seeder'
 
 const warning = "Create .env file with your database credentials.";
 
-function getConnectionCredentials() : Sequelize {
+function getConnectionCredentials(): Sequelize {
     const sslMode: boolean = process.env.NODE_ENV == 'production';
 
     return new Sequelize(
@@ -21,13 +22,14 @@ function getConnectionCredentials() : Sequelize {
         });
 }
 
-function getConnection() : Sequelize {
+const getConnection = async (): Promise<Sequelize> =>  {
 
     const connectionCredentials = getConnectionCredentials();
 
     try {
-        connectionCredentials.authenticate();
+        await connectionCredentials.authenticate();
         console.log('Connection has been established successfully.');
+
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
@@ -36,4 +38,3 @@ function getConnection() : Sequelize {
 }
 
 export default getConnection;
-
---
+++ Application/services/server/src/api/models/seeder.ts
@@ -0,0 +1,5 @@
+import associationSeeder from './association-seeder'
+
+export default async () => {
+    await associationSeeder();
+}
