aa9f1781869c8c8b470334385b9558b4259a94a8
maria.hendrikx <maria.hendrikx@student.uhasselt.be>
2020-04-22 12:21:52 +0200

    added filter to map

--- Application/services/client-mobile/src/pages/sector-responsible/map/MapPage.tsx
+++ Application/services/client-mobile/src/pages/sector-responsible/map/MapPage.tsx
@@ -9,7 +9,7 @@ import { IonButton,
     IonList, 
     IonItem, 
     IonLabel,
-    IonText, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent, IonAvatar, IonIcon } from '@ionic/react';
+    IonText, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent, IonAvatar, IonIcon, IonSelect, IonSelectOption } from '@ionic/react';
 import { Link } from 'react-router-dom';
 import { caretDown, call, mail } from 'ionicons/icons';
 
@@ -28,6 +28,11 @@ class MapPage extends Component<any> {
     super(props);
   }
 
+  state={
+    selected_sector: -1, //if -1 = selected all sectors
+    data_posts: []
+  }
+
   static defaultProps = {
     center: {
       lat: 50.9307,
@@ -40,7 +45,78 @@ class MapPage extends Component<any> {
     this.props.fetchPosts();
   }
 
+  renderButtons(){
+    return(
+      <IonButton className="mapButton">
+        <IonSelect
+          interface="popover"  
+          value={this.state.selected_sector} placeholder={"Sector " + this.state.selected_sector} onIonChange={e => this.handleSectorChange(e.detail.value)}>
+            {this.props.arePostsFetched.posts_sectors.map((sector: number) => {
+                return <IonSelectOption value={sector}>Sector {sector}</IonSelectOption>
+            })}
+            <IonSelectOption value={-1}>Alle sectors</IonSelectOption>
+
+        </IonSelect>
+      </IonButton>
+    )
+    
+  }
+
+  handleSectorChange(sector: number){
+    let new_data
+    if(sector !== -1){
+      new_data = this.props.arePostsFetched.posts_data.filter((element: any) => {
+        return element.sector_id === sector
+      })     
+    }else{
+      new_data = this.props.arePostsFetched.posts_data
+    }
+   
+    this.setState({...this.state, selected_sector: sector, data_posts: new_data});
+  }
+
   renderPosts(){
+    if(this.state.data_posts.length <= 0){
+      this.setState({...this.state, data_posts: this.props.arePostsFetched.posts_data});
+      return this.props.arePostsFetched.posts_data.map((data: any, index: number) =>{
+        console.log(data);
+        if(data.problem === false){
+          return (
+            <NormalMarker 
+            lat={data.latitude} 
+            lng={data.longitude} />
+          )
+        } else{
+          return (
+            <ProblemMarker 
+            lat={data.latitude} 
+            lng={data.longitude} />
+          )
+        }
+        
+      })
+    } else {
+      return this.state.data_posts.map((data: any, index: number) =>{
+        console.log(data);
+        if(data.problem === false){
+          return (
+            <NormalMarker 
+            lat={data.latitude} 
+            lng={data.longitude} />
+          )
+        } else{
+          return (
+            <ProblemMarker 
+            lat={data.latitude} 
+            lng={data.longitude} />
+          )
+        }
+        
+      })
+    }  
+  }
+
+  renderContent(){
     if(this.props.loading == true){
       return <div>Loading...</div>
     } else {
@@ -48,28 +124,25 @@ class MapPage extends Component<any> {
         if(this.props.arePostsFetched.length <= 0){
           return <div> No messages found. </div>
         } else{
-          console.log("PROBLEEEMMMM", this.props.arePostsFetched)
+          return (
+            <div className="GoogleMaps">
+            {this.renderButtons()}
 
-          return this.props.arePostsFetched.posts_data.map((data: any, index: number) =>{
-            console.log(data);
-            if(data.problem === false){
-              return (
-                <NormalMarker 
-                lat={data.latitude} 
-                lng={data.longitude} />
-              )
-            } else{
-              return (
-                <ProblemMarker 
-                lat={data.latitude} 
-                lng={data.longitude} />
-              )
-            }
-            
-          })
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDyMg3eezA_aKnVp1Hvsya23xwxCey32JA' }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >        
+              {this.renderPosts()}
+
+            </GoogleMapReact>
+          </div>
+          )
+          
         }
       }
     }
+    
   }
 
   render(){
@@ -82,17 +155,8 @@ class MapPage extends Component<any> {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-        <div className="GoogleMaps">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDyMg3eezA_aKnVp1Hvsya23xwxCey32JA' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-
-        {this.renderPosts()}
-
-        </GoogleMapReact>
-        </div>
+          {this.renderContent()}
+       
         </IonContent>
       </IonPage>
     )
