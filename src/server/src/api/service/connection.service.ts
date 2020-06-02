8af4abdfa89654efd316d44419c22c9877476569
Michiel Swaanen <michiel.swaanen.gsm@gmail.com>
2020-06-02 10:07:58 +0200

    [DEMO] User location simulator

--- src/server/src/api/service/connection.service.ts
+++ src/server/src/api/service/connection.service.ts
@@ -1,25 +1,22 @@
 import UserModel from "../models/user.model";
 
-export const listen = async (seconds : number) => {
+export const startUserConnectionListener = (seconds : number) => {
 
-    setTimeout(() => {
+    setInterval(async () => {
+        console.log("Listening to user connection... @" + new Date());
+        const users = await UserModel.findAll({where: {is_connected: true}});
 
+        for (let user of users) {
+            const lastUpdateAt : number = new Date(user.updated_at).getTime();
+            const tenMinutes = 10 * 60 * 1000;
+
+            if((lastUpdateAt + tenMinutes) < new Date().getTime()) {
+                // TODO: Possibility to add extra functionality with a "Is shift still active check"
+
+                await UserModel.update({is_connected: false}, {where: {id: user.id}});
+            }
+        }
     }, seconds * 1000)
 
-    const users = await UserModel.findAll({where: {is_connected: true}});
-
-    for (let user of users) {
-        const lastUpdateAt : number = new Date(user.updated_at).getTime();
-        const tenMinutes = 10 * 60 * 1000;
-
-        if((lastUpdateAt + tenMinutes) < new Date().getTime()) {
-            // TODO: Possibility to add extra functionality with a "Is shift still active check"
-
-            await UserModel.update({is_connected: false}, {where: {id: user.id}});
-        }
-    }
-}
-
-export const stopService = () => {
 
 }
